a49aab7c3837256096bafa6700299b638a15ef1a
ngattusohw <ngattusohw@gmail.com>
2018-02-18 21:41:25 -0500

    Finishing lab4

--- Lab4/app.js
+++ Lab4/app.js
@@ -1,14 +1,51 @@
+/*
+Nick Gattuso
+I pledge my honor that I have abided by the stevens honor system
+
+ */
+
+
+
 const todoItems = require("./todo.js");
 
 async function main() {
 	try{
-	    const createdTask = await todoItems.createTask("My Second Task", "This is the first thing I need to do today");
-	    console.log(createdTask);
+	    // const createdTask = await todoItems.createTask("My Second Task", "This is the first thing I need to do today");
+	    // console.log(createdTask);
+
+	    // const removeAllItems = await todoItems.getAllTasks();
+	    // for(var i = 0; i < removeAllItems.length; i++){
+	    // 	let x = todoItems.removeTask(removeAllItems[i]._id);
+	    // }
+	    //console.log(allItems);
+
+	    // const getItemWithId = await todoItems.getTask("ae8de7a0-14e2-11e8-884a-abf1d22c0d55");
+	    // console.log(getItemWithId);
+
+	    // const completeTask = await todoItems.completeTask("ae8de7a0-14e2-11e8-884a-abf1d22c0d55");
+	    // console.log(completeTask);
+	    
+
+	    const createdTask1 = await todoItems.createTask("Ponder Dinosaurs","Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like?"); 
+	    console.log(createdTask1);
+
+	    const createdTask2 = await todoItems.createTask("Play Pokemon with Twitch TV","Should we revive Helix?");
 
 	    const allItems = await todoItems.getAllTasks();
 	    console.log(allItems);
+
+	    const deletedItem = await todoItems.removeTask(allItems[0]._id);
+
+	    const allRemainingItems = await todoItems.getAllTasks();
+	    console.log(allRemainingItems);
+
+	    const completedTask = await todoItems.completeTask(allRemainingItems[0]._id);
+	    console.log(completedTask);
+
+
+
 	}catch(e){
-		console.log(e);
+		console.log("Failure::  " + e);
 	}
 }
 
--- Lab4/todo.js
+++ Lab4/todo.js
@@ -21,10 +21,9 @@ const createTask = async function createTask(title, description){
         const insertInfo = await todoCollection.insertOne(return_task);
         if (insertInfo.insertedCount === 0) throw "Could not add the new task!";
 
-        return insertInfo;
+        return return_task;
 
     }
-
 
 }
 
@@ -35,21 +34,45 @@ const getAllTasks = async function() {
 }
 
 const getTask = async function(id) {
-    if (!id) throw "You must provide an id to search for";
+    if (!id||arguments.length!==1||typeof id!== 'string') throw "You must provide a valid id to search for";
     const todoCollection = await todoItems();
     const todo = await todoCollection.findOne({ _id: id });
     if (todo === null) throw "No todo item with that id";
     return todo;
 }
 
-const completeTask = function(id) {
+const completeTask = async function(id) {
+    if (!id||arguments.length!==1||typeof id!== 'string') throw "You must provide a valid id to search for";
+    const todoCollection = await todoItems();
+
+    var updatedtoDo = await getTask(id);
+    updatedtoDo.completed = true;
+    updatedtoDo.completedAt = new Date();
+
+    const updatedInfo = await todoCollection.replaceOne(
+      { _id: id },
+      updatedtoDo
+    );
+
+    if (updatedInfo.modifiedCount === 0) {
+      throw "could not update todo successfully";
+    }
+
+    return await this.getTask(id);
 
 }
 
-const removeTask = function(id) {
+const removeTask = async function(id) {
+    if(!id||arguments.length!==1||typeof id!== 'string') throw "You must provide an id to remove!";
+    const todoCollection = await todoItems();
+    const deletionInfo = await todoCollection.removeOne({ _id: id });
 
+    if (deletionInfo.deletedCount === 0) {
+      throw "Could not delete todo with id of ${id}";
+    }else{
+        return true;
+    }
 }
-
 
 module.exports = {
     firstName: "Nicholas", 
