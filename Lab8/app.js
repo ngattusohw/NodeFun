b70fe49777553c78bb1e912e6394afe1a849d206
ngattusohw <ngattusohw@gmail.com>
2018-03-24 21:42:30 -0400

    Finishing lab8

--- Lab8/app.js
+++ Lab8/app.js
@@ -7,6 +7,8 @@ const palidrome = require("./js/isPalindrome.js");
 const exphbs = require("express-handlebars");
 
 app.use("/", static);
+// app.use("/", express.static(__dirname));
+// app.use("/", express.static(__dirname + "public/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
@@ -18,18 +20,39 @@ app.get('/', function(req, res) {
 });
 
 app.post('/result', function(req, res){
-	var the_text = req.body["text-to-test"].toLowerCase().replace(/[.,\/#!\'$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '').replace("\'", "");
-	var isBool = palidrome.isPalindrome(the_text);
-	// res.render("palidrome/result", 
-	// 	{
-	// 		title: "The Palindrome Results!",
-	// 		status: var_status,
-	// 		status-message: status_message,
-	// 		palidrome-response: response
-	// 	});
+	if(req.body["text-to-test"]){
+		var the_text = req.body["text-to-test"].toLowerCase().replace(/[.,\/#!\'\`\’$%\^&\*;?:{}=\-_`~()]/g,"").replace(/\s+/g, '').replace("\'", "");
+		var isBool = palidrome.isPalindrome(the_text);
 
-
-	console.log(isBool);
+		if(isBool){
+			//render success
+			res.render("palidrome/result", 
+			{
+				title: "The Palindrome Results!",
+				status: "success",
+				status_message: "This string is a Palindrome!",
+				palidrome_response: req.body["text-to-test"]
+			});
+		}else{
+			//render fail
+			res.render("palidrome/result", 
+			{
+				title: "The Palindrome Results!",
+				status: "failure",
+				status_message: "This string is NOT a Palindrome!",
+				palidrome_response: req.body["text-to-test"]
+			});
+		}
+	}else{
+		//no input entered, send the client a 400
+		res.status(400);
+		res.render("palidrome/result", 
+		{
+			title: "The Palindrome Results!",
+			status: "error",
+			palidrome_response: "ERROR: Palindrome field should not be empty!"
+		});
+	}
 });
 
 app.listen(3000, () => {
